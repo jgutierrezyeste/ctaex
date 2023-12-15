677b8b465c3166ad161953dd53c8f014aca0489c
aranzazucastillo <aranzazu.castillo@fotex.es>
2023-12-15 11:57:17 +0100

    Añadir patologia_concreta1 a tabla expedientes

---
+++ database/migrations/2023_12_15_105439_update_expedientes_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('expedientes', function (Blueprint $table) {
+            $table->text('patologia_concreta1')->after('patologia_concreta')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('expedientes', function (Blueprint $table) {
+            $table->dropColumn('patologia_concreta1');
+        });
+    }
+};
