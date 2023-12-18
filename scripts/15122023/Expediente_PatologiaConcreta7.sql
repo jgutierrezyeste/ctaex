2128ba0d4e447adf68862395096c043e0b30bed0
jgutierrezyeste <39293515+jgutierrezyeste@users.noreply.github.com>
2023-12-18 09:53:34 +0100

    Corrección de valores en Patologias_concretas y relaciones de expediente con Patologias_concretas

--- scripts/15122023/Expediente_PatologiaConcreta7.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO expediente_patologia_concreta(expediente_id,patologia_concreta_id) VALUES (646, (SELECT id FROM patologia_concreta WHERE nombre = ' HIPOACUSIA'));
---
+++ scripts/18122023/Correcciones_patologias_concretas.sql
@@ -0,0 +1,8 @@
+UPDATE `ctaex`.`patologia_concreta` SET `nombre` = 'AUSENCIA CONTROL ESFERA SEXUAL' WHERE (`id` = '28');
+UPDATE `ctaex`.`patologia_concreta` SET `nombre` = 'HEMIPARESIA IZQUIERDA ' WHERE (`id` = '172');
+UPDATE `ctaex`.`patologia_concreta` SET `nombre` = 'LUXACION CONGENITA CADERA IZQUIERDA' WHERE (`id` = '209');
+DELETE FROM `ctaex`.`patologia_concreta` WHERE (`id` = '170');
+DELETE FROM `ctaex`.`patologia_concreta` WHERE (`id` = '173');
+DELETE FROM `ctaex`.`patologia_concreta` WHERE (`id` = '174');
+
+
