253ddc77e736ca42813be64cfab7a350fd77a7c5
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-01-18 14:15:02 +0100

    boton back

--- resources/views/components/regreso.blade.php
+++ resources/views/components/regreso.blade.php
@@ -1,7 +1,7 @@
 
 <form method="GET" action="{{route($ruta) }}">
     @csrf
-    <img src="{{asset('assets/img/boton_inicio.png')}}" alt="75" width="75">
+   
     <input type="submit" class="btn-regreso" align="center" value = "{{$regreso}}" />
 </form>
 
