3c429dd157dc6d0d7793557c2104691e204e6c94
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-03-11 13:29:20 +0100

    clave_foranea_rendiciones

---
+++ database/migrations/2024_02_29_105338_update_clave_foranea_rendiciones_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('rendiciones', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('expediente_id')->change();
+            $table->foreign('expediente_id')->references('id')->on('expedientes');
+
+            
+        });
+    }
+};
