30b361101a538df6d7eb78968e779c12feb5bc9f
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-03-11 12:58:19 +0100

    clave foranea administracion_bienes

---
+++ database/migrations/2024_02_27_132937_update_clave_foranea_administracion_bienes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('administracion_bienes', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('estado_id')->change();
+            $table->foreign('estado_id')->references('id')->on('estados');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+       
+        Schema::table('administracion_bienes', function (Blueprint $table) {
+            $table->dropForeign('administracion_bienes_estado_id_foreign');
+        });
+    }
+};
