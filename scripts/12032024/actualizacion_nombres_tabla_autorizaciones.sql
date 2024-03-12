3f3bf23982d35b3a41153cd8fd65d3844c546edd
jgutierrezyeste <39293515+jgutierrezyeste@users.noreply.github.com>
2024-03-12 09:42:10 +0100

    Actualización del campo nombre de la tabla autorizaciones

---
+++ scripts/12032024/actualizacion_nombres_tabla_autorizaciones.sql
@@ -0,0 +1,5 @@
+UPDATE `ctaex`.`autorizaciones` SET `nombre` = 'USO DE ARMAS' WHERE (`id` = '3');
+UPDATE `ctaex`.`autorizaciones` SET `nombre` = 'PERMISO DE CONDUCIR' WHERE (`id` = '4');
+UPDATE `ctaex`.`autorizaciones` SET `nombre` = 'OPERACION ESTETICA' WHERE (`id` = '5');
+UPDATE `ctaex`.`autorizaciones` SET `nombre` = 'CERTIFICADO DE OPERABILIDAD' WHERE (`id` = '6');
+UPDATE `ctaex`.`autorizaciones` SET `nombre` = 'USO DE REDES SOCIALES' WHERE (`id` = '8');
