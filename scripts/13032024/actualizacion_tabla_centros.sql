2758b4a0f6455c1f506f3fe19a04f5d4498834d4
jgutierrezyeste <39293515+jgutierrezyeste@users.noreply.github.com>
2024-03-13 11:01:28 +0100

    Actualización tabla centros

---
+++ scripts/13032024/actualizacion_tabla_centros.sql
@@ -0,0 +1,14 @@
+UPDATE centros SET tipo_via = 'AVENIDA' WHERE tipo_via = 'AVDA.';
+UPDATE centros SET tipo_via = 'CALLE' WHERE tipo_via = 'C';
+UPDATE centros SET tipo_via = 'CALLE' WHERE tipo_via = 'C/';
+UPDATE centros SET tipo_via = 'CALLE' WHERE tipo_via = 'calle';
+UPDATE centros SET tipo_via = 'CAMINO' WHERE tipo_via = 'CMNO.';
+UPDATE centros SET tipo_via = 'PASEO' WHERE tipo_via = 'PSO';
+UPDATE centros SET tipo_via = 'PASEO' WHERE tipo_via = 'PSO.';
+UPDATE centros SET tipo_via = 'PLAZA' WHERE tipo_via = 'PZA.';
+UPDATE centros SET tipo_via = 'RONDA' WHERE tipo_via = 'RDA.';
+UPDATE centros SET tipo_via = 'TRAVESIA' WHERE tipo_via = 'TRAV,';
+UPDATE centros SET tipo_via = 'TRAVESIA' WHERE tipo_via = 'TRAV.';
+UPDATE centros SET tipo_via = 'URBANIZACION' WHERE tipo_via = 'URB.';
+
+UPDATE centros SET tipo_via_id = (SELECT tv.id FROM tipos_via tv WHERE tv.nombre = tipo_via);
