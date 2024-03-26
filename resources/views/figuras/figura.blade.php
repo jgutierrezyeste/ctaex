de16fd80ad56c62ecd65135c43a3a980621eaf30
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-03-26 12:26:15 +0100

    apm_figuras

--- resources/views/figuras/figura.blade.php
+++ resources/views/figuras/figura.blade.php
@@ -1,44 +1,29 @@
-@extends ('layouts.landing')
-@section('title','Figuras')
-@section('subtitle','Figuras')
+@php
+    $ruta_regreso="index_apm";
+    $subtitulo="Figuras";
+    $rutaEdicion="figuras.edit";
+    $rutaDelete="figuras.destroy";
+    $rutaAniadir="figuras.create"; 
+    $campos=['nombre'];
 
-@section('content')
-<div class="table table-responsive">   
-<table class="table table-sm table-bordered">
-        <thead >
-            
-            <td>Figuras</td>
-           
-            <td></td>
-            <td></td>
-            
-        </thead>
-        <tbody>
+@endphp
+
+<x-mostrarmenusapm :titulo="$subtitulo" :rutaRegreso="$ruta_regreso" :rutaAniadir="$rutaAniadir" :campos="$campos" >    
+    @section('cuerpo')
             @foreach ($figuras as $figura)  
-                <tr> 
-                    <td>{{ $figura->nombre }}</td>
-                    
-                    <td>
-                        <form method="GET" action="{{route('figuras.edit',$figura->id) }}">
-                        @csrf
-                        <input type="submit" value = "EDIT" />
-                        </form>
-                    </td>
-                    <td>
-                        <form method="POST" action="{{route('figuras.destroy',$figura->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value = "DELETE" />
-                        </form>
-                    </td>
-                </tr>
-            @endforeach     
-        </tbody>
-    </table>
-</div>
-    
-    <form method="GET" action="{{route('figuras.create') }}">
-        @csrf
-        <input type="submit" value = "AÑADIR FIGURA" />
-    </form>
-@endsection
+            <tr> 
+                @foreach ($campos as $campo)
+                <td class="columna_datos">{{ $figura->$campo }}</td>
+                @endforeach
+                <td>
+                    <x-boton_editar :ruta="$rutaEdicion" :elemento="$figura"></x-boton_editar>
+                </td>
+                <td>
+                    <x-boton_eliminar :ruta="$rutaDelete" :elemento="$figura"></x-boton_editar>
+                </td>
+            </tr>
+            
+            @endforeach  
+                
+    @endsection
+</x-mostrarmenusapm>
