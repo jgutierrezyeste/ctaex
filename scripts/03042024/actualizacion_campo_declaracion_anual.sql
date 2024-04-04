30e8ff68ade5024ed52648ed7a7636093fbb2ef6
jgutierrezyeste <39293515+jgutierrezyeste@users.noreply.github.com>
2024-04-04 08:20:51 +0200

    actualización campos declaracion_Anual y ultima_rdc_aprobada

---
+++ scripts/03042024/actualizacion_campo_declaracion_anual.sql
@@ -0,0 +1,15 @@
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = '' WHERE (`declaracion_anual` = 'SEPTIEMBRE 2019FEBRERO 2024');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = 'SEPTIEMBRE  2023' WHERE (`declaracion_anual` = 'SEPTIEMBRE 2023');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = '' WHERE (`declaracion_anual` = 'NO TIENE DECLARACIONES');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = 'MAYO 2023' WHERE (`declaracion_anual` = 'MAYO  2023');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = 'MARZO 2022' WHERE (`declaracion_anual` = 'marzo2022');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = 'JUNIO 2023' WHERE (`declaracion_anual` = 'JUNIO  2023');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = 'ENERO 2022' WHERE (`declaracion_anual` = 'ENERO2022');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = '' WHERE (`declaracion_anual` = 'DICIEMBRE 2021FEBRERO 2024');
+UPDATE `ctaex`.`expedientes` SET `declaracion_anual` = 'SEPTIEMBRE 2023' WHERE (`declaracion_anual` = 'SEPTIEMBRE  2023');
+
+
+
+
+
+
---
+++ scripts/03042024/actualizacion_campo_ultima_rdc_aprobada.sql
@@ -0,0 +1,4 @@
+UPDATE `ctaex`.`expedientes` SET `ultima_rdc_aprobada` = 'JUNIO 2020' WHERE (`ultima_rdc_aprobada` = 'JNIO 2020');
+UPDATE `ctaex`.`expedientes` SET `ultima_rdc_aprobada` = 'AGOSTO 2020' WHERE (`ultima_rdc_aprobada` = 'GOSTO 2020');
+UPDATE `ctaex`.`expedientes` SET `ultima_rdc_aprobada` = '' WHERE (`ultima_rdc_aprobada` = 'NO HACER RENDICION');
+UPDATE `ctaex`.`expedientes` SET `ultima_rdc_aprobada` = 'MARZO 2019' WHERE (`ultima_rdc_aprobada` = 'MARZO2019');
