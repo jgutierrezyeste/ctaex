0af7c8c5cae8b2c7879866903a1841f92d3c07b6
jgutierrezyeste <39293515+jgutierrezyeste@users.noreply.github.com>
2024-04-29 14:40:03 +0200

    Actualizar num_expte_intranet de la tabla expedientes

---
+++ database/migrations/2024_02_07_114301_create_revisiones_table_3.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('revisiones', function (Blueprint $table) {
+            $table->engine ='InnoDB';
+            $table->id();
+            $table->bigInteger('expediente_id')->nullable()->default(null);
+            $table->bigInteger('iniciador_id')->nullable()->default(null);
+            $table->date('fecha_revision')->nullable()->default(null);
+            $table->date('fecha_recepcion')->nullable()->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('revisiones');
+    }
+};
---
+++ database/migrations/2024_02_29_110641_update_clave_foranea_revisiones_table_3.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('revisiones', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('expediente_id')->change();
+            $table->foreign('expediente_id')->references('id')->on('expedientes');
+
+            
+            $table->unsignedBigInteger('iniciador_id')->change();
+            $table->foreign('iniciador_id')->references('id')->on('iniciador_procedimientos');
+            
+            
+        });
+    }
+};
