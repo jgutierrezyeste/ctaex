6cfd2d5351269ccda56e9198aa6568c67421d85b
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-05-02 11:42:01 +0200

    script actualizacion juzgado_id e inclusion nuevos letrados

---
+++ scripts/02052024/nuevos_letrados.sql
@@ -0,0 +1,3 @@
+INSERT INTO `letrados` (`nombre`, `apellido1`, `apellido2`) VALUES ('MARIA BELEN', 'ROBLEÑO', 'MARIANO');
+INSERT INTO `letrados` (`nombre`, `apellido1`, `apellido2`) VALUES ('SOLEDAD', 'CHACON', 'ANDRADE');
+INSERT INTO `letrados` (`nombre`, `apellido1`, `apellido2`) VALUES ('PEDRO ANTONIO', 'CLAROS', 'MIGUEL');
---
+++ scripts/30042024/actualizacion_juzgado_id_faltantes_defensas_judiciales.sql
@@ -0,0 +1,9 @@
+UPDATE defensas_judiciales set juzgado_id=26 where num_expte='10/01';
+UPDATE defensas_judiciales set juzgado_id=34 where num_expte='11/19' or num_expte='56/12';
+UPDATE defensas_judiciales set juzgado_id=8 where num_expte='15/23' or  num_expte='23/16' or  num_expte='24/14' or  num_expte='25/19' or  num_expte='27/09' or  num_expte='30/20' or  num_expte='31/20' or  num_expte='34/23' or  num_expte='37/09' or  num_expte='40/11' or  num_expte='50/19' or  num_expte='53/22' ;
+UPDATE defensas_judiciales set juzgado_id=1 where num_expte='33/18';
+UPDATE defensas_judiciales set juzgado_id=42 where num_expte='82/23';
+UPDATE defensas_judiciales set juzgado_id=21 where num_expte='83/23';
+UPDATE defensas_judiciales set juzgado_id=20 where num_expte='84/23';
+
+
