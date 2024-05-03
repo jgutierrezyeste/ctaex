7a0c1612a2aedb4ff2fa64d8126a9254730ee48c
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-05-03 12:42:07 +0200

    script correccion datos_personales

---
+++ scripts/03052024/correcion_expedientes_datos_personales.sql
@@ -0,0 +1,3 @@
+update `expedientes_intranet` set `apellido1`='GUDIÑO CACERES' WHERE (`numero_documento` = '08744222J');
+UPDATE `expediente_datos_personales` SET `apellido1` = 'DOS SANTOS DOS SANTOS' WHERE (`numero_documento` = 'X1818081T');
+UPDATE `expediente_datos_personales` SET `apellido1` = 'GUDIÑO CACERES' WHERE (`numero_documento` = '08744222J');
