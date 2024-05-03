6cccffb0cbca5cbc7a7f78dcbac61f6bf098a91d
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-05-03 13:07:46 +0200

    eliminacion expediente repetido

---
+++ scripts/03052024/correcion_expediente_manuel_garrote_arroyo.sql
@@ -0,0 +1,3 @@
+UPDATE `fallecidos` SET `expediente_id` = '1108', `nombre` = 'MANUEL' WHERE (`apellidos` = 'GARROTE ARROYO');
+DELETE FROM `expedientes_intranet` WHERE (`id` = '774') and nombre='GOMEZ NEVADO' AND apellido1='GARROTE ARROYO';
+
