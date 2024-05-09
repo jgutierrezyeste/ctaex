730754a4963f1124e00a086d59012b0e42c512bc
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-05-09 12:26:22 +0200

    correcciones expediente_datos_personales

---
+++ scripts/09052024/correcciones_expedientes_datos_personales.sql
@@ -0,0 +1,4 @@
+UPDATE `expediente_datos_personales` SET `numero_documento` = 'Y1090272V' WHERE (`id` = '479') and nombre='COSTI ALEXANDRU';
+update `expediente_datos_personales` set documento_identificativo_id=4 where documento_identificativo_id=5;
+UPDATE `expediente_datos_personales` SET `domicilio_id` = '2' WHERE (`id` = '71') and nombre='JUANA' and apellido1='BUENO' and apellido2='GUISADO';
+UPDATE `expediente_datos_personales` SET `domicilio_id` = '104' WHERE (`id` = '378') and nombre='MARIA PILAR' and apellido1='DIEZ' and apellido2='SANTIAGO';
