c27229e399174dd1723d7dc5aa793dad0999eea9
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-05-10 12:48:21 +0200

    expediente_patlogia_concreta

---
+++ database/migrations/2024_02_29_111733_update_clave_foranea_expediente_patologia_concreta_table2.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{public function up(): void
+    {
+        Schema::table('expediente_patologia_concreta', function (Blueprint $table) {
+            
+            
+            $table->unsignedBigInteger('expediente_id')->change();
+            $table->foreign('expediente_id')->references('id')->on('expedientes');
+
+            $table->unsignedBigInteger('patologia_concreta_id')->change();
+            $table->foreign('patologia_concreta_id')->references('id')->on('patologia_concreta');
+            
+            
+        });
+    }
+};
