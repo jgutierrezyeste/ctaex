586fae2f72019bcd03d3c59c7c9a7c5272838be6
aranzazucastillo <aranzazu.castillo@fotex.es>
2024-05-10 12:41:38 +0200

    clave foranea patologia concreta

---
+++ database/migrations/2024_02_28_133603_update_clave_patologia_concreta2_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('patologia_concreta', function (Blueprint $table) {
+            
+            $table->unsignedBigInteger('patologia_general_id')->change();
+            $table->foreign('patologia_general_id')->references('id')->on('patologia_general');
+
+            
+        });
+    }
+};
